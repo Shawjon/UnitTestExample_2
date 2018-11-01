055fc0229036a54921c283b9481da8584ca0db3c
Jon Shaw <Jonshaw@quickenloans.com>
2018-11-01 15:01:46 -0400

    made the starting code

---
+++ UnitTestExample_2/ReturnArray.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestExample_2
+{
+    public class ReturnArray
+    {
+        public string PrintVar { get; set; }
+        public ReturnArray()
+        {
+        }
+
+        public static void ConsolePrintToScreen()
+        {
+            ReturnArray test = new ReturnArray();
+            Console.WriteLine(test.PrintVar);
+        }
+        public int[] ReturnSameArray(int[] array)
+        {
+            return array;
+        }
+      
+
+        public bool CheckEven(int[] intArray)
+        {
+            foreach (var element in intArray)
+            {
+                if ((element % 2) != 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UnitTestExample_2;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void ReturnSameArray_CompareArrays_True()
+        {
+            //arrange 
+            int[] intArray = { 2, 4, 6, 8, 10 };
+            ReturnArray returnArray = new UnitTestExample_2.ReturnArray();
+            //act
+            int [] returnedArray = returnArray.ReturnSameArray(intArray);
+            //assert
+            CollectionAssert.AreEqual(intArray, returnedArray);
+        }
+
+        [TestMethod]
+        public void CheckEven_CheckArrayElements_True()
+        {
+            //arrange 
+            int[] intArray = { 2, 4, 6, 8, 10 };
+            ReturnArray returnArray = new ReturnArray();
+            //act
+            bool isArrayEven = returnArray.CheckEven(intArray);
+            //assert
+            Assert.IsTrue(isArrayEven);
+
+        }
+        [TestMethod]
+        public void PrintVar_WriteToScreen_True()
+        {
+            //arrange 
+            ReturnArray returnArray = new ReturnArray();
+            //act
+            returnArray.PrintVar = "This test prints out a variable";
+            //assert
+            Assert.AreEqual("This test prints out a variable", returnArray.PrintVar);
+
+        }
+    }
+}
